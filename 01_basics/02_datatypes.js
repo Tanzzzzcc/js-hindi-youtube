833b4b3f6ea4f2ee72a826fb9c9f58b002e7c1ee
TANISHA SRIVASTAVA <tanz19458@gmail.com>
2026-04-20 20:05:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat js code as a newer version
+//alert(3+3) // bcz we are using node thwerefor eni workkrega using node js and browser.
+
+
+
+
+console.log("tanisha")
+let name="tanzu"
+let age=18
+let isloggedin=false
+//number=2 to 53
+//bigint
+//string =""
+//boolean =t/f
+//null=stamdalone value, khali hai
+//undefined value ni assign
+//symbol unique
+
+
+//object
+console.log(typeof "tanisha") //string
+    console.log(typeof undefined) //undefined hi
+
+    console.log(typeof null)//type is object
